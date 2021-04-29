0d7d433b6aca12c1a1b247d484eeb710b569b583
Kristin <Kristineliseboyd13@gmail.com>
2021-04-29 14:43:42 -0700

    new api

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def get_hello_world():
+    my_response = "Hello, world"
+    return my_response 
+
+
+@hello_world_bp.route("/hello-world/JSON", methods=["GET"])
+def hello_world_json():
+    return {
+        "name": "Kristin",
+        "message": "Hellerr",
+        "hobbies": ["Animals", "Plants", "Cooking", "Organizing"]
+    }, 200
+
+
